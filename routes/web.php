57162224826c274946eb084aec50256a862e3d9c
ifs21011-itdel <archicosemb@gmail.com>
2023-11-20 02:22:06 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ExperienceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [ExperienceController::class,'index']);
+Route::get('/', [ExperienceController::class,'index']);
+
+Route::resource('/experience', 'ExperienceController');
