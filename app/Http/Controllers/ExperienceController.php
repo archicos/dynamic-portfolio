bb84de3e6270fc7947ae7ccfa24873fc1c8d9283
ifs21011-itdel <archicosemb@gmail.com>
2023-11-20 02:21:51 +0700

    Create ExperienceController.php

---
+++ app/Http/Controllers/ExperienceController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Experience;
+use Illuminate\Http\Request;
+
+class ExperienceController extends Controller
+{
+    public function index()
+    {
+        $experience = Experience::get();
+        return view('welcome', ['experienceList'=> $experience]);
+    }
+
+    public function create()
+    {
+        dd('berhasil');
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
