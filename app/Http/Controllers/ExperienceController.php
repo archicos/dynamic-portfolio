396c8c1d0abca941befeef6b8db9c691486b8973
ifs21011-itdel <archicosemb@gmail.com>
2023-11-20 16:17:13 +0700

    Update ExperienceController.php

--- app/Http/Controllers/ExperienceController.php
+++ app/Http/Controllers/ExperienceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Experience;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class ExperienceController extends Controller
 {
@@ -42,18 +43,45 @@ class ExperienceController extends Controller
         //
     }
 
-    public function edit(string $id)
+    public function edit($id)
     {
-        //
+        $experience = Experience::findOrFail($id);
+
+        // if (!$experience) {
+        //     abort(404); // Atau tindakan lain sesuai kebutuhan Anda
+        // }
+
+        return view('welcome', compact('experience'));
     }
 
     public function update(Request $request, string $id)
     {
-        //
+        $this->validate($request, [
+            'title'=> 'required',
+            'description'=> 'required',
+            'link'=> 'required',
+        ]);
+
+        $experience = Experience::findOrFail($id);
+        $experience->title = $request->input('title');
+        $experience->description = $request->input('description');
+        $experience->link = $request->input('link');
+        $experience->save();
+
+        return redirect('/')->with('success','Data Updated!');
     }
 
     public function destroy(string $id)
     {
-        //
+        $deletedExperience = Experience::findOrFail($id);
+
+        $deletedExperience->delete(); 
+        
+        if($deletedExperience) {
+            Session::flash('status', 'success');
+            Session::flash('message', 'Student successfully deleted!');
+        }
+
+        return redirect('/')->with('success','Data Deleted!');
     }
 }
