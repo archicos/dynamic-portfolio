c37de15df7c1c49f5bf7aa72a6709f33245c3225
ifs21011-itdel <archicosemb@gmail.com>
2023-11-23 20:13:43 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,26 +1,31 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Http\Controllers\ExperienceController;
-use App\Http\Controllers\SoftskillController;
+use App\Models\Project;
+use App\Models\Softskill;
+use App\Models\Experience;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\SoftskillController;
+use App\Http\Controllers\ExperienceController;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $experienceController = new ExperienceController();
-        $softskillController = new SoftskillController();
+        $experience = Experience::get();
+        $softskill = Softskill::get();
+        $project = Project::get();
 
-        $experienceData = $experienceController->index();
-        $softskillData = $softskillController->index();
+        return view('welcome', ['experienceList' => $experience, 'softskillList' => $softskill, 'projectList'=> $project]);
+    }
+    public function home()
+    {
+        $experience = Experience::get();
+        $softskill = Softskill::get();
+        $project = Project::get();
 
-        $data = [
-            "experienceList" => $experienceData, 
-            "softskillList" => $softskillData
-        ];
-
-        return view('welcome', $data);
+        return view('homepage', ['experienceList' => $experience, 'softskillList' => $softskill, 'projectList'=> $project]);
     }
 }
+
