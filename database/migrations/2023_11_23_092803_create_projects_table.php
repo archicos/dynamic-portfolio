18022e78c20e40759dcb4654ccf5a97efa1ea64e
ifs21011-itdel <archicosemb@gmail.com>
2023-11-23 20:13:55 +0700

    Create 2023_11_23_092803_create_projects_table.php

---
+++ database/migrations/2023_11_23_092803_create_projects_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('project_name', 100);
+            $table->string('category', 100);
+            $table->string('image', 100)->nullable();
+            $table->string('description', 1000)->nullable();
+            $table->string('badge', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
